ae2aed00efbaafb048e2c8f5a6e70a737ed0a22d
prince singh <np01cp4a190098@islingtoncollge.edu.np>
2021-03-20 18:25:41 +0545

    docs is added

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import sys
 from PyQt5.QtWidgets import *
 
 sys.path.insert(0, os.path.join( os.path.dirname(__file__), "..", ".." ))
-
+# importing calculatorWindows
 from princelab.calc_window import CalculatorWindow
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
